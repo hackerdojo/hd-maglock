663ee66c6f3fd6abb014646d90251b062fa19b19
Charlie <root@maglock.local>
2010-07-29 23:33:07 +0000

    Adding new files

--- serialController.py
+++ serialController.py
@@ -2,39 +2,60 @@ import serial
 import time
 import urllib
 import json
+import sys
+
+relayfile = serial.Serial('/dev/cuau0', baudrate=9600)
+print "\nHacker Dojo RFID Entry System v0.2\n"
 
 def relayOn():
+  print '[RELAY] On'
   relayfile.setDTR(True)
-  #print 'on'
 
 def relayOff():
+  print '[RELAY] Off'
   relayfile.setDTR(False)
-  #print 'off'
-
-relayfile = serial.Serial('/dev/ttyUSB0', baudrate=9600)
 
 def getUsers():
-  userURL = urllib.urlopen('http://signup.hackerdojo.com/api/rfid')
-  data = userURL.read()
-  userURL.close()
-  userData = json.loads(data)
+  try:
+    userURL = urllib.urlopen('/tmp/rfid')
+    data = userURL.read()
+    userURL.close()
+  except:
+    fatal("Unable to open RFID file",sys.exc_info()[0])
+  try:
+    userData = json.loads(data)
+  except:
+    fatal("Unable to parse JSON",sys.exc_info()[0])
+  if len(userData) < 1:
+    fatal("Number of RFID tags too small")
   return userData
 
-userData = getUsers()
-for user in userData:
-  print user['rfid_tag']
+def fatal(msg,err):
+  print "\n"
+  print "[ERROR] " + str(msg)
+  print "[ERROR] " + str(err)
+  sys.exit(0)
 
 while True:
-  key = raw_input('RFID?').strip()
-  print key
-  for user in userData:
-     print user['rfid_tag']
-     if key == user['rfid_tag']:
-        foundUser = user
-  
-  if foundUser is not None:
-    print 'hello ' + foundUser['username']
-    relayOff()
-    time.sleep(3)
-    relayOn()
+  key = raw_input('RFID> ').strip()
+  if key in ["exit","exit()","quit","quit()"]:
+    print "[EXIT] Exiting"
+    sys.exit(0)  
+  if key:
+    print "[DEBUG] I just scanned " + key
+    userData = getUsers()
+    print "[DEBUG] Comparing to " + str(len(userData)) + " RFIDs"
     foundUser = None
+    for user in userData:
+       if key == user['rfid_tag']:
+          foundUser = user
+    if foundUser:
+      print '[FOUND] ' + foundUser['username']
+      relayOff()
+      time.sleep(3)
+      relayOn()
+      foundUser = None
+    else:
+      print '[NOTFOUND] Sorry, RFID key not found'
+      
+      
---
+++ updater.py
@@ -0,0 +1,32 @@
+import serial
+import time
+import urllib
+import json
+import sys
+
+def fatal(msg,err):
+  print "\n"
+  print "[ERROR] " + str(msg)
+  print "[ERROR] " + str(err)
+  sys.exit(0)
+
+try:
+  userURL = urllib.urlopen('http://signup.hackerdojo.com/api/rfid')
+  data = userURL.read()
+  userURL.close()
+except:
+  fatal("Unable to connect to server",sys.exc_info()[0])
+try:
+  userData = json.loads(data)
+except:
+  fatal("Unable to parse JSON",sys.exc_info()[0])
+if len(userData) < 1:
+  fatal("Number of RFID tags too small")
+
+try:
+  FILE = open("/tmp/rfid","w")
+  FILE.writelines(data)
+  FILE.close()
+except:
+  fatal("Unable to write RFID data",sys.exc_info()[0])
+  
